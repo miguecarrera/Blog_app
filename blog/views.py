034d371779b52f47eb8de54793cb7c6d291859ca
miguecarrera <ahola6479@gmail.com>
2022-02-22 12:19:40 +0100

    Usuarios

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,31 @@
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
-from .models import Post
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from . models import Post
 
 
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
 
+
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
-    context_object_name = 'anything_you_want'
+
+
+class BlogCreateView(CreateView): # new
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'body']
+    template_name = 'post_edit.html'
+
+class BlogDeleteView(DeleteView): # new
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
